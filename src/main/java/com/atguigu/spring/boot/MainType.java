e5e781f359a4bf97d74493633f65e90157b2e59a
harry <harry@atguigu.com>
2020-07-18 11:44:13 +0800

    project init

---
+++ src/main/java/com/atguigu/spring/boot/MainType.java
@@ -0,0 +1,13 @@
+package com.atguigu.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainType {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainType.class, args);
+    }
+
+}
